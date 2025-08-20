5124a353001e9b21f3964089a320bd10281007ad
Reethu P <144982034+REETHUPERIYASAMY@users.noreply.github.com>
2025-08-20 22:23:13 +0530

    Update index.js

---
+++ src/data/index.js
@@ -0,0 +1,7 @@
+// src/data/index.js
+export { questions_tamil_beginner } from './questions_tamil_beginner';
+export { questions_tamil_intermediate } from './questions_tamil_intermediate';
+export { questions_tamil_advanced } from './questions_tamil_advanced';
+export { questions_english_beginner } from './questions_english_beginner';
+export { questions_english_intermediate } from './questions_english_intermediate';
+export { questions_english_advanced } from './questions_english_advanced';
