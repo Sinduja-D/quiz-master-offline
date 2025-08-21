1ab5bf74bbb900bc72729cd27eabf7de5dcce843
REETHUPERIYASAMY <preethu.0306@gmail.com>
2025-08-21 16:55:25 +0530

    updated data

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,7 @@
 // src/data/index.js
-export { questions_tamil_beginner } from './questions_tamil_beginner';
-export { questions_tamil_intermediate } from './questions_tamil_intermediate';
-export { questions_tamil_advanced } from './questions_tamil_advanced';
-export { questions_english_beginner } from './questions_english_beginner';
-export { questions_english_intermediate } from './questions_english_intermediate';
-export { questions_english_advanced } from './questions_english_advanced';
+export { questions_tamil_beginner } from './questions_tamil_beginner.js';
+export { questions_tamil_intermediate } from './questions_tamil_intermediate.js';
+export { questions_tamil_advanced } from './questions_tamil_advanced.js';
+export { questions_english_beginner } from './questions_english_beginner.js';
+export { questions_english_intermediate } from './questions_english_intermediate.js';
+export { questions_english_advanced } from './questions_english_advanced.js';
